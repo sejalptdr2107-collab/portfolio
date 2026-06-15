4793c40c081d8f842d5a309dc3828650e3a92219
sejalptdr2107-collab <sejalptdr2107@gmail.com>
2026-06-15 23:18:38 +0530

    Add files via upload

---
+++ story.py
@@ -0,0 +1,9 @@
+first = input("Who is the first character: ")
+second = input("Who is the second character: ")
+third = input("Where did they find the map: ")
+fourth = input("What is the name of the forest: ")
+fifth = input("What was inside the forest: ")
+sixth = input("What did they decide to do with the treasure: ")
+
+print(f"One day, {first} and {second} found an old map near {third}. The map led them through the {fourth} Forest to a hidden cave.")
+print(f"Inside the cave, they discovered a chest filled with {fifth}. They decided to {sixth} and returned home safely.")
